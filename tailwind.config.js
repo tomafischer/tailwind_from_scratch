6567c6da2d5486a0fa7353cc77b9895fd95932e9
Tom <tom.a.fischer@gmail.com>
2024-03-06 19:26:14 -0500

    finished first chapter

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,27 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-blue': '#1992d4',
+      },
+      spacing: {
+        '72': '18rem',
+ 
+      },  
+    },
   },
   variants: {
     backroundColor: ['responsive', 'hover', 'focus', 'active'],
     fontSize: ['responsive', 'hover']
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: ["./public/**/*.{html,js}"],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_]+/g) || []
+    })
+  ],
 }
 
